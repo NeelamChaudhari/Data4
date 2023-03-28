b394842e2fd48c4b5d7c292832087c03a3f7be11
Admin <Admin@DESKTOP-B5L4A4H>
2023-03-28 17:01:01 +0530

    Hello

---
+++ src/Java_Logical/Java_Class.java
@@ -0,0 +1,16 @@
+package Java_Logical;
+
+public class Java_Class 
+{
+	public static void main(String[] args)
+	{
+		
+		int a=10;
+		int b=20;
+		int c;
+		c=a+b;
+		System.out.println(c);
+		
+	}
+
+}
